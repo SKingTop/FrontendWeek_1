056c75969c6df104b4635313d6a3e6ca04111e6c
SKing <secret.king.1996@gmail.com>
2019-02-02 19:38:01 +0600

    02.02.2019/19:37

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,9 @@
 $('.nav-menu-btn').on('click', function(){
 			$(this).closest('.nav-menu').toggleClass('menu-open');
 });
+$('.footerAncor').on('click',function(event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1200);
+    });
